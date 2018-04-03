e7df2844eaef20af764b22c9bdd33abd6e2321b4
Kimberly Bird <kimberly.j.bird@gmail.com>
2018-04-02 19:21:03 -0500

    car function for lightning

---
+++ main.js
@@ -0,0 +1,11 @@
+// create a function called 'go' that takes 2 arguments: direction (forwards, backwards, etc.) and speed (mph). The function, when invoked, will print out the following in your console (for example): "The car is moving forward, at 25 mph."
+
+
+// my version of the function
+function go (direction, speed) {
+    console.log(`The car is moving ${direction} at ${speed} mph`)
+}
+
+go("forward", 34)
+go("backward", 5)
+go("forward", 90)
